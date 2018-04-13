038f7ea1fdd6a862b0ad45c402f757cb204adb46
SRavit1 <38190530+SRavit1@users.noreply.github.com>
2018-04-12 23:07:10 -0700

    Create Chain_AL.java
    new class (subclass of ArrayList) to store AminoAcid sequence

---
+++ Chain_AL.java
@@ -0,0 +1,11 @@
+public class Chain_AL extends java.util.ArrayList{
+	public String toString() {
+		String output = "";
+		for (int i = 0; i < this.size(); i++) {
+			AminoAcid current = (AminoAcid) this.get(i);
+			output += current.name;
+			output += "\n";
+		}
+		return output;
+	}
+}
