6cb9d45281c9dc3c05c11485a2740b87be1eeda5
SRavit1 <38190530+SRavit1@users.noreply.github.com>
2018-04-13 21:53:05 -0700

    AminoAcid - Version 1.5

--- AminoAcid.java
+++ AminoAcid.java
@@ -1,12 +1,14 @@
 import java.awt.*;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class AminoAcid extends Canvas{ 
+public class AminoAcid extends Label{
+	
 	public boolean mobile;
 	public boolean in_chain;
 	
 	public String name;
-	public String Polarity;
+	public String short_name;
+	public boolean Polarity;
 	public double HydropathyIndex;
 	public String SideChainCharge;
 	
@@ -16,31 +18,42 @@ public class AminoAcid extends Canvas{
 	int x;
 	JPanel panel;
 	
-	AminoAcid (String name, String Polarity, double HydropathyIndex, String SideChainCharge) {
+	AminoAcid (String name, Boolean Polarity, double HydropathyIndex, String SideChainCharge, String short_name) {
+		//This constructor sets the fields of the object according to the parameters
 		this.name = name;
 		this.Polarity = Polarity;
 		this.HydropathyIndex = HydropathyIndex;
 		this.SideChainCharge = SideChainCharge;
+		
+		this.short_name = short_name;
 	}
-	public String toString () {
-		return name + "\nPolarity: " + Polarity +
-				"\nHydropathy Index: " + HydropathyIndex +
-				"\nSide Chain Charge: " + SideChainCharge + "\n";
-	}
+	
 	public void setup (int r, int g, int b, int x, JPanel panel) {
+		//Setting the value of the fields of AminoAcid equal to the parameters passed in
 		this.r = r;
 		this.g = g;
 		this.b = b;
 		this.x = x;
 		this.panel = panel;
+		
+		//Setting the bounds and color of the object according to the parameters
 		setBounds(x, 50, 50, 50);
 		setBackground(new Color(r, g, b));
 		panel.add(this);
-		addMouseMotionListener(new GUI.box_dragged());
-		addMouseListener(new GUI.box_dragged());
+		
+		//Adding mouse listener and mouse motion listener to AminoAcid
+		addMouseMotionListener(new GUI.box_mouselistener());
+		addMouseListener(new GUI.box_mouselistener());
+		
+		//Labeling the AminoAcid with its abbreviation
+		Font font = new Font("Arial", Font.CENTER_BASELINE, 14);
+		this.setAlignment(Label.CENTER);
+		this.setFont(font);
+		this.setText(short_name);
 	}
 	public AminoAcid clone () {
-		AminoAcid clone = new AminoAcid(name, Polarity, HydropathyIndex, SideChainCharge);
+		//This method creates and returns a new AminoAcid with most of the same fields
+		AminoAcid clone = new AminoAcid(name, Polarity, HydropathyIndex, SideChainCharge, short_name);
 		clone.r = r;
 		clone.g = g;
 		clone.b = b;
